c0c95c3ab45c6ce8ee8dd8e4dbacf0eedc98d084
matt-hu <matt.hu1@outlook.com>
2023-05-29 14:39:55 +0100

    Add more tests for complete coverage

--- tests/locoStore.test.ts
+++ tests/locoStore.test.ts
@@ -1,5 +1,5 @@
 import { Direction } from '@trainlink-org/trainlink-types';
-import { Loco } from '../src';
+import { Loco, LocoStoreBase } from '../src';
 
 describe('Test creating Loco', () => {
     test('Create Loco with parameters', () => {
@@ -43,5 +43,49 @@ describe('Test Loco functionality', () => {
         loco.speed = 10;
         loco.direction = Direction.forward;
         expect(loco.toString()).toBe('Test 5 - 10 Forward');
+    });
+    test('Test fromJson', () => {
+        const json = JSON.stringify(loco);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        expect(Loco.fromJson(JSON.parse(json))).toEqual(loco);
+    });
+});
+
+class LocoStore extends LocoStoreBase {}
+
+describe('Test LocoStoreBase', () => {
+    const store = new LocoStore();
+    test('Add a new loco and fetch it', async () => {
+        const loco = new Loco('Test', 23);
+        store.add(loco);
+        await expect(store.getLoco('Test')).resolves.toEqual(loco);
+        await expect(store.getLoco(23)).resolves.toEqual(loco);
+        expect(store.getAllLocos()).toContain(loco);
+    });
+    test('Fetch a non-existant Loco', async () => {
+        await expect(store.getLoco(1)).rejects.toMatch('not found');
+        await expect(store.getLoco('Name')).rejects.toMatch('not found');
+    });
+    test('Delete a loco from the store', () => {
+        expect(store.deleteLoco(1)).toBe(false);
+        const loco = new Loco('', 24);
+        store.add(loco);
+        expect(store.deleteLoco(24)).toBe(true);
+    });
+    test('Update a loco', async () => {
+        const loco = new Loco('', 25);
+        store.add(loco);
+        store.updateLoco(25);
+        await expect(store.getLoco(25)).resolves.toEqual(loco);
+        store.updateLoco(25, 'UpdateTest', 26);
+        await expect(store.getLoco(26)).resolves.toBeDefined();
+        const updatedLoco = await store.getLoco(26);
+        expect(updatedLoco.name).toBe('UpdateTest');
+    });
+    test('Test toString method', () => {
+        const newStore = new LocoStore();
+        const loco = new Loco('Test', 42);
+        newStore.add(loco);
+        expect(newStore.toString()).toMatch('Test 42 - 0 Forward');
     });
 });
